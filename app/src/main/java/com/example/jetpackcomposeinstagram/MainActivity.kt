c4640f79cc17221950a5f206241b1fdb3c71bd26
Arifischbein <ariifischbein@gmail.com>
2024-06-14 18:27:54 -0300

    login screen - compose

--- app/src/main/java/com/example/jetpackcomposeinstagram/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposeinstagram.ui.LoginScreen
 import com.example.jetpackcomposeinstagram.ui.theme.JetpackComposeInstagramTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +20,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetpackComposeInstagramTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            JetpackComposeInstagramTheme(darkTheme = false) {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+//                    containerColor = Color.Transparent
+                ) { innerPadding ->
+                    LoginScreen(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeInstagramTheme {
-        Greeting("Android")
     }
 }
